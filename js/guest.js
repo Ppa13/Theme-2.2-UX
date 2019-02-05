7fa0f6e91b46c2ba02d8abc2b59e5e8c372e8c55
Paula P <patrunjel.paula@gmail.com>
2019-02-05 17:37:47 +0100

    Guest page layout done, no functionalities added

--- js/guest.js
+++ js/guest.js
@@ -1,1 +1,9 @@
-guest.js
+// Open login pop-up
+function openLogin(){
+ 	document.getElementById("account-form-section").style.display="block";
+}
+
+// Close login pop-up
+function closeLogin(){
+  	document.getElementById("account-form-section").style.display="none";
+}
